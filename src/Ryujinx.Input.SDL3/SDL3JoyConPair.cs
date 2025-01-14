b01dcd1963a81a6ca81b9f9618d297908b0398e0
madwind <madwind.cn@gmail.com>
2025-01-15 00:27:57 +0800

    update

--- src/Ryujinx.Input.SDL3/SDL3JoyConPair.cs
+++ src/Ryujinx.Input.SDL3/SDL3JoyConPair.cs
@@ -1,6 +1,5 @@
 using Ryujinx.Common.Configuration.Hid;
 using Ryujinx.Common.Configuration.Hid.Controller;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;

0d86378561d3d68a5be3c8c94598743e4e63a256
madwind <madwind.cn@gmail.com>
2025-03-02 12:22:00 +0800

    Update SDL to tag release-3.2.6

--- src/Ryujinx.SDL3-CS/external/update.ps1
+++ src/Ryujinx.SDL3-CS/external/update.ps1
@@ -1,8 +1,9 @@
-$env:TAG_NAME="release-3.2.4"
+$env:TAG_NAME="release-3.2.6"
 
 git submodule update --init --recursive
 git -C src/Ryujinx.SDL3-CS/external/SDL fetch --tags
 git -C src/Ryujinx.SDL3-CS/external/SDL checkout tags/$env:TAG_NAME
 git add src/Ryujinx.SDL3-CS/external/SDL
+git add src/Ryujinx.SDL3-CS/external/update.ps1
 git commit -m "Update SDL to tag $env:TAG_NAME"
  
